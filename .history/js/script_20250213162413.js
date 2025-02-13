2d9e3819a18bf99b2aec4439bed8935e354c2720
John Doe <leohamon9@gmail.com>
2025-02-13 16:28:25 +0100

    script de deconnexion = supression cookies

--- .history/js/script_20250213162413.js
+++ .history/js/script_20250213162413.js
@@ -1,13 +1,6 @@
 const tokenCookieName = "accesstoken";
 const signoutBtn = document.getElementById("signout-btn")
 
-signoutBtn.addEventListener("click", signout);
-
-function signout(){
-    eraseCookie(tokenCookieName);
-    window.location.reload();
-}
-
 
 function setToken(token){
     setCookie(tokenCookieName, token, 7);
